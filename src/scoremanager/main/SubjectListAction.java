11517e5ea798fc1324b9e233d0d818c33c7c1ede
'2302065' <'tky2302065@stu.o-hara.ac.jp'>
2024-04-25 12:45:41 +0900

    科目登録作成出来ました 岸田

---
+++ src/scoremanager/main/SubjectListAction.java
@@ -0,0 +1,34 @@
+package scoremanager.main;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import tool.Action;
+
+public class SubjectListAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		HttpSession session = req.getSession();//セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		//ローカル変数1
+		SubjectDao subjectDao = new SubjectDao(); // 科目Dao
+
+		//DBからデータ取得3
+		List<String>list = subjectDao.filter(teacher.getSchool());
+
+		//レスポンス値をセット6
+		req.setAttribute("list",list);
+
+		// JSPへフォワード 7
+		req.getRequestDispatcher("subject_create_done.jsp").forward(req, res);
+
+	}
+
+}
