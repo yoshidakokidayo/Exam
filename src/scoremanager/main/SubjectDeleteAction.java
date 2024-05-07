0fe8bf03683819bd1a2a8e2d149c6e3ae56c5233
'2302065' <'tky2302065@stu.o-hara.ac.jp'>
2024-05-07 09:45:39 +0900

    科目削除完成　岸田

---
+++ src/scoremanager/main/SubjectDeleteAction.java
@@ -0,0 +1,48 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectDeleteAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//ローカル変数の宣言1
+		HttpSession session = req.getSession(); // セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");
+		String subject_cd = "";
+		String subject_name = "";
+		Subject subject= new Subject();
+		SubjectDao subjectDao = new SubjectDao ();
+
+
+		//リクエストパラメーターの取得2
+		subject_cd = req.getParameter("cd");
+
+		//DBからのデータ取得3
+		//科目の詳細データを取得
+		subject = subjectDao.get(subject_cd , teacher.getSchool());
+
+		//ビジネスロック4
+		subject_name = subject.getName();
+
+		// レスポンス値をセット 6
+		// リクエストに科目コードをセット
+		req.setAttribute("subject_cd", subject_cd);
+		// リクエストに科目名をセット
+		req.setAttribute("subject_name", subject_name);
+
+		// JSPへフォワード 7
+		req.getRequestDispatcher("sbject_delete.jsp").forward(req, res);
+
+	}
+
+}
